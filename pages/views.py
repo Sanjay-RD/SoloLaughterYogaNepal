384d46050c7bf236f75ec39eef152821a3469a01
Sanjay-RD <rishidevsanjay9@gmail.com>
2021-08-30 22:58:03 +0545

    About us, Gallery & Contact pages

--- pages/views.py
+++ pages/views.py
@@ -1,5 +1,13 @@
 from django.shortcuts import render
 
 # Create your views here.
+def aboutus(request):
+    return render(request,'pages/aboutus.html')
+
+
+def gallery(request):
+    return render(request,'pages/gallery.html')
+
+
 def contact(request):
     return render(request,'pages/contact.html')
